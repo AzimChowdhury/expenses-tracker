a93b5911e145703b88593bdfc88ed44091311b4d
Azim Chowdhury <azimchy994@gmail.com>
2024-02-07 21:08:47 +0600

    done

--- src/app/(DashboardLayout)/layout.tsx
+++ src/app/(DashboardLayout)/layout.tsx
@@ -3,9 +3,12 @@ import Sidebar from "@/components/Sidebar";
 import { useState } from "react";
 import Category from "@/components/Category";
 import Expenses from "@/components/Expenses";
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+
 
 const DashboardLayout = ({ children }: Readonly<{ children: React.ReactNode; }>) => {
-
+    const [drawerState, setDrawerState] = useState<boolean>(false)
     const [value, setValue] = useState<number>(0);
 
     const handleChange = (newValue: number) => {
@@ -16,8 +19,19 @@ const DashboardLayout = ({ children }: Readonly<{ children: React.ReactNode; }>)
 
 
     return (
-        <div className="dashboard   min-w-screen min-h-screen bg-[#32A7E2] p-7 flex">
+        <div className=" relative  min-w-screen min-h-screen bg-[#32A7E2] p-7 flex">
+
             <Sidebar handleChange={handleChange} value={value} />
+
+            <Button className="absolute top-0 left-0" onClick={() => setDrawerState(!drawerState)}>{'left'}</Button>
+            {/* small device sidebar */}
+            <Drawer
+                anchor={"left"}
+                open={drawerState}
+            >
+                hello
+                <Button onClick={() => setDrawerState(!drawerState)}>{'left'}</Button>
+            </Drawer>
 
             {
                 value === 1 && <Category />
